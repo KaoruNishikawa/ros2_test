fa61ac5cfd271204e805944f703b669f3834ba82
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 20:55:55 +0900

    test

--- launch/launch_node_num_test_pub.py
+++ launch/launch_node_num_test_pub.py
@@ -1,14 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
+def generate_launch_description(num):
+    return LaunchDescription([
+        Node(
+            package='ros2_test',
+            node_executable='node_num_pub',
+            parameters=[
+                {'node_num': num},
+            ]
+        ),
+    ])
+
+if __name__ == '__main__':
     for i in range(5):
-        return LaunchDescription([
-            Node(
-                package='ros2_test',
-                node_executable='node_num_pub',
-                parameters=[
-                    {'node_num': i},
-                ]
-            ),
-        ])
+        generate_launch_description(i)
