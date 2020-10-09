86eb5ce022be2bdab2a4f8db4c8b0bec688de3e0
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 21:07:16 +0900

    beautifully align

--- launch/launch_node_num_test_pub.py
+++ launch/launch_node_num_test_pub.py
@@ -2,7 +2,15 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    a = []
+    nodes = []
     for i in range(5):
-        a.append(Node(package='ros2_test', node_executable='node_num_pub', parameters=[{'node_num': i}]))
-    return LaunchDescription(a)
+        nodes.append(
+            Node(
+                package='ros2_test',
+                node_executable='node_num_pub',
+                parameters=[
+                    {'node_num': i}
+                ]
+            )
+        )
+    return LaunchDescription(nodes)
