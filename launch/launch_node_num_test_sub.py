3d2e9ec6347953b45ed7aaf48c7d193601ad725c
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 21:29:38 +0900

    fold up

--- launch/launch_node_num_test_sub.py
+++ launch/launch_node_num_test_sub.py
@@ -2,12 +2,15 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='ros2_test',
-            node_executable='node_num_sub',
-            parameters=[
-                {'node_num': 1},
-            ]
-        ),
-    ])
+    nodes = []
+    for i in range(5):
+        nodes.append(
+            Node(
+                package='ros2_test',
+                node_executable='node_num_sub',
+                parameters=[
+                    {'node_num': i}
+                ]
+            )
+        )
+    return LaunchDescription([nodes)
