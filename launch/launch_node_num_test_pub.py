82fed591c9e81f5fe589d015d48785ede5af688f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 21:02:28 +0900

    fix typo

--- launch/launch_node_num_test_pub.py
+++ launch/launch_node_num_test_pub.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description(num):
+def generate_launch_description():
     a = []
     for i in range(5):
         a.append(Node(package='ros2_test', node_executable='node_num_pub', parameters=[{'node_num': i}]))
