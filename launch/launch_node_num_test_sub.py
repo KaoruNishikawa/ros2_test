cf9b2bb00614319b495d03b6402db88c3168732f
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 20:37:52 +0900

    add node number testing

---
+++ launch/launch_node_num_test_sub.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros2_test',
+            node_executable='subscriber',
+            parameters=[
+                {'node_num': 1},
+            ]
+        ),
+    ])
---
+++ ros2_test/node_num_test_pub.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+node_name = "delay_test_pub"
+
+import rclpy
+import time
+from std_msgs.msg import Float64
+
+class communication_test_pub(object):
+
+    def __init__(self):
+        self.node = rclpy.create_node(node_name)
+        self.node.declare_parameter('node_num')
+        self.num = str(self.node.get_parameter('node_num').value)
+        self.my_pub = self.node.create_publisher(Float64, "/test/node_num_"+self.num, 1)
+        timer_period = 1
+        self.node.create_timer(timer_period, self.my_publisher)
+
+    def my_publisher(self):
+        msg = Float64()
+        msg.data = float(time.time())
+        self.my_pub.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    pub = communication_test_pub()
+    rclpy.spin(pub.node)
+
+    pub.node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
