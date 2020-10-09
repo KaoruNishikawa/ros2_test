b3334596c71281b4388ef01ef658244d3e1e7776
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 09:20:05 +0000

    reflect

---
+++ ros2_test/communication_test_sub.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+node_name = "communication_test_sub"
+
+import rclpy
+import time
+from std_msgs.msg import Float64
+
+class communication_test_sub(object):
+    def __init__(self):
+        self.node = rclpy.create_node(node_name)
+        subscriber = self.node.create_subscription(Float64, "/test/time", self.sub_callback, 1)
+
+    def sub_callback(self, timer):
+        current_time = float(time.time())
+        send_time = float(timer.data)
+        delta = current_time - send_time
+        with open("/home/amigos/Documents/test_timer.txt", "a") as f:
+            f.write(str(delta)+'\n')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    subscriber = communication_test_sub()
+    rclpy.spin(subscriber.node)
+
+    subscriber.node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'ros2_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='amigos',
+    maintainer_email='amigos@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publisher='+package_name+'.communication_test_pub:main',
+            'subscriber='+package_name+'.communication_test_sub:main',
+        ],
+    },
+)
