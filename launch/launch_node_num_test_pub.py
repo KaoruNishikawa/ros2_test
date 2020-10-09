9cf1a1c8113cac2433e36a482b0fb716dac7ea11
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-09 21:01:40 +0900

    desperate test

--- launch/launch_node_num_test_pub.py
+++ launch/launch_node_num_test_pub.py
@@ -2,16 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description(num):
-    return LaunchDescription([
-        Node(
-            package='ros2_test',
-            node_executable='node_num_pub',
-            parameters=[
-                {'node_num': num},
-            ]
-        ),
-    ])
-
-if __name__ == '__main__':
+    a = []
     for i in range(5):
-        generate_launch_description(i)
+        a.append(Node(package='ros2_test', node_executable='node_num_pub', parameters=[{'node_num': i}]))
+    return LaunchDescription(a)
