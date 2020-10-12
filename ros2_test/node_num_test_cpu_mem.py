b5da485f05065c91375b37c02f572e1a7c3e17f6
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-12 11:04:33 +0900

    brush up

--- ros2_test/node_num_test_cpu_mem.py
+++ ros2_test/node_num_test_cpu_mem.py
@@ -34,7 +34,7 @@ class cpu_mem_checker(object):
         with open(f"{os.environ['HOME']}/Documents/cpu_used.txt", "a") as f:
             f.write(str(res_cpu)+'\n')
         # test
-        self.node.get_logger().info('mem: %s, err: %s' % (res_mem, res_cpu))
+        # self.node.get_logger().info('mem: %s, err: %s' % (res_mem, res_cpu))
         return
 
 
