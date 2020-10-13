72215a5b46970c550cbfb3a925b7e6732d4a1e94
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-13 18:09:45 +0900

    fix procedure

---
+++ ros2_test/node_num_test_sub_m.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+node_name = "delay_test_sub"
+
+import rclpy
+import time
+import os
+from std_msgs.msg import Float64
+
+class node_num_test_sub(object):
+    
+    def __init__(self):
+        self.node = rclpy.create_node(node_name)
+        self.node.declare_parameter('node_num')
+        self.num = str(self.node.get_parameter('node_num').value)
+        with open(f"{os.environ['HOME']}/Documents/node_num_test/test_node_num_{self.num}.txt", "a") as self.f:
+            subscriber = self.node.create_subscription(Float64, "/test/node_num_"+self.num, self.sub_callback, 1)
+
+    def sub_callback(self, timer):
+        current_time = float(time.time())
+        send_time = float(timer.data)
+        delta = current_time - send_time
+        self.f.write(str(delta)+'\n')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    subscriber = node_num_test_sub()
+    rclpy.spin(subscriber.node)
+
+    subscriber.node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
