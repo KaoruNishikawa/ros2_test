5d3849a5366e45177db5c6b1a8ee35ee7d222530
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-13 19:23:35 +0900

    bug fix

--- launch/launch_node_num_test_m.py
+++ launch/launch_node_num_test_m.py
@@ -9,7 +9,7 @@ def generate_launch_description():
                 package='ros2_test',
                 node_executable='node_num_pub',
                 parameters=[
-                    {'node_num': i+1}
+                    {'node_num': i}
                 ]
             )
         )
@@ -18,7 +18,7 @@ def generate_launch_description():
                 package='ros2_test',
                 node_executable='dummy_sub',
                 parameters=[
-                    {'node_num': i+1}
+                    {'node_num': i}
                 ]
             )
         )
