8d2d292071627c56372bbaeb6372a7f6c33977d6
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-23 19:02:30 +0900

    bug fix

--- ros2_test/node_num_test_cpu_mem.py
+++ ros2_test/node_num_test_cpu_mem.py
@@ -11,24 +11,24 @@ class cpu_mem_checker(object):
 
     def __init__(self):
         self.node = rclpy.create_node(node_name)
-        timer_period = 1
+        timer_period = 2
         self.f_mem = open(f"{os.environ['HOME']}/Documents/mem_used.txt", "w")
         self.f_cpu = open(f"{os.environ['HOME']}/Documents/cpu_used.txt", "w")
         self.node.create_timer(timer_period, self.checker)
 
     def checker(self):
         try:
-            command_mem_1 = 'top -b -n1'
-            command_mem_2 = 'grep buff/cache'
-            res1_mem = subprocess.Popen(command_mem_1.split(' '), stdout=subprocess.PIPE)
-            res2_mem = subprocess.Popen(command_mem_2.split(' '), stdin=res1_mem.stdout, stdout=subprocess.PIPE)
-            res_mem = res2_mem.communicate()[0]
+            top_command = 'top -b -n1'
+            command_mem = 'grep buff/cache'
+            res_top = subprocess.Popen(top_command.split(' '), stdout=subprocess.PIPE)
+            res_mem = subprocess.Popen(command_mem.split(' '), stdin=res_top.stdout, stdout=subprocess.PIPE)
+            res_mem = res_mem.communicate()[0]
             # command_cpu_1 = 'top -b -n1'
-            command_cpu_2 = 'grep Cpu'
+            command_cpu = 'grep Cpu'
             # res1_cpu = subprocess.Popen(command_cpu_1.split(' '), stdout=subprocess.PIPE)
             # res2_cpu = subprocess.Popen(command_cpu_2.split(' '), stdin=res1_cpu.stdout, stdout=subprocess.PIPE)
-            res2_cpu = subprocess.Popen(command_cpu_2.split(' '), stdin=res1_mem.stdout, stdout=subprocess.PIPE)
-            res_cpu = res2_cpu.communicate()[0]
+            res_cpu = subprocess.Popen(command_cpu.split(' '), stdin=res_top.stdout, stdout=subprocess.PIPE)
+            res_cpu = res_cpu.communicate()[0]
         except:
             res_mem = ""
             res_cpu = ""
