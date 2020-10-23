61fa3d69d03d39008343897b494a0d38ff9a31f0
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-23 18:32:47 +0900

    brush up

--- shellscript/node_test.sh
+++ shellscript/node_test.sh
@@ -17,12 +17,18 @@ sleep 1s
 for i in 1 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300 320 340 360 380 400
 do
     node_num=$i
-    echo start testing with $node_num node\(s\)
+    # echo start testing with $node_num node\(s\)
     sed -i "s/for.*/for i in range\($node_num\)\:/" $launch_file
     sleep 1s
     timeout 100s ros2 launch $launch_file
-    sleep 15s
+    sleep 30s
+    cd ~/Documents
+    mv -i cpu_used.txt cpu_used_$i.txt
+    mv -i mem_used.txt mem_used_$i.txt
+    mv -i test_node_num_0.txt delay_${i}_node.txt
+    cd ~/ros2/src/ros2_test/launch
+    sleep 5s
 done
 
 # back to initial directory
-cd ./../shellscript
+cd ../shellscript
