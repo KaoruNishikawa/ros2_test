b54a1f8952d67bd0b4f5e9d87b399b80d3e0190b
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-23 18:07:37 +0900

    bug fix

--- shellscript/node_test.sh
+++ shellscript/node_test.sh
@@ -5,8 +5,8 @@ launch_file=launch_node_only_test.py
 
 # build
 cd ~/ros2
-colcon build --symlink-install
-. ./install/setup.bash
+# colcon build --symlink-install
+# . ./install/setup.bash
 
 # launch
 cd src/ros2_test/launch
@@ -15,15 +15,7 @@ sleep 5s
 # 
 node_num=1
 echo start testing with $node_num node\(s\)
-sed -i "s/\sfor.*/\tfor i in range\($node_num\)\:/" $launch_file
-sleep 1s
-timeout 100s ros2 launch $launch_file
-sleep 15s
-
-# 
-node_num=20
-echo start testing with $node_num node\(s\)
-sed -i "s/\sfor.*/\tfor i in range\($node_num\)\:/" $launch_file
+sed -i "s/\sfor.*/    for i in range\($node_num\)\:/" $launch_file
 sleep 1s
 timeout 100s ros2 launch $launch_file
 sleep 15s
