1addafc1970a44d4a696af0d094aa87206df788c
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-26 14:23:41 +0900

    fix possible bug

--- shellscript/node_test.sh
+++ shellscript/node_test.sh
@@ -15,13 +15,12 @@ sleep 1s
 # 
 for node_num in 1 20 40 60 80 100 120 140 160 180 200 220 240 260 280 300 320 340 360 380 400
 do
-    # node_num=$i
-    # echo start testing with $node_num node\(s\)
     sed -i "s/for.*/for i in range\($node_num\)\:/" $launch_file
     sleep 1s
     timeout -s SIGINT 100s ros2 launch $launch_file
     sleep 30s
     cd ~/Documents
+    node_num=`printf "%03g" $node_num`
     mv -i cpu_used.txt cpu_used_$node_num.txt && :
     mv -i mem_used.txt mem_used_$node_num.txt && :
     mv -i test_node_num_0.txt delay_${node_num}_node.txt && :
