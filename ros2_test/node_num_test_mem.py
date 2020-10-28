9d126310747bb2ff0a44328fc8181c468d98e523
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-10-28 10:35:08 +0900

    change method for data acquisition

--- ros2_test/node_num_test_mem.py
+++ ros2_test/node_num_test_mem.py
@@ -5,6 +5,7 @@ node_name = "mem_checker"
 import rclpy
 from rclpy.node import Node
 import os
+import psutil
 import subprocess
 from std_msgs.msg import Float64
 
@@ -13,17 +14,18 @@ class mem_checker(Node):
     def __init__(self):
         super().__init__(node_name)
         # self.node = rclpy.create_node(node_name)
-        timer_period = 1
+        timer_period = 2
         self.f_mem = open(f"{os.environ['HOME']}/Documents/mem_used.txt", "w")
         self.create_timer(timer_period, self.checker)
 
     def checker(self):
         try:
-            top_command = 'top -b -n1'
-            command_mem = 'grep buff/cache'
-            res_mem1 = subprocess.Popen(top_command.split(' '), stdout=subprocess.PIPE)
-            res_mem2 = subprocess.Popen(command_mem.split(' '), stdin=res_mem1.stdout, stdout=subprocess.PIPE)
-            res_mem = res_mem2.communicate()[0]
+            # top_command = 'top -b -n1'
+            # command_mem = 'grep buff/cache'
+            # res_mem1 = subprocess.Popen(top_command.split(' '), stdout=subprocess.PIPE)
+            # res_mem2 = subprocess.Popen(command_mem.split(' '), stdin=res_mem1.stdout, stdout=subprocess.PIPE)
+            # res_mem = res_mem2.communicate()[0]
+            res_mem = psutil.virtual_memory()
         except:
             res_mem = ""
         self.f_mem.write(str(res_mem)+'\n')
