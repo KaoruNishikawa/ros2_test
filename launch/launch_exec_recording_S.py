1b2f83263021a46e913172d0c4ca4893ebba749b
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-11-02 20:19:42 +0900

    bug fix

---
+++ launch/launch_exec_recording_S.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    ld.add_action(
+        Node(
+            package = 'ros2_test',
+            executable = 'exec_recording_nodes_S',
+        )
+    )
+    return ld
