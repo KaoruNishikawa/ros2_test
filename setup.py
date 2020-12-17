32a57c6f265d14318fd0cbf1b2e9a190cdf8bbba
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-12-17 09:34:41 +0000

    make executor package

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ executors = [
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[package_name, 'executor'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
