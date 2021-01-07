8dd04d14336fa54c1fb023e6515b592889e6d92d
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-07 19:44:26 +0900

    fix repeatedly executed substitution

--- shellscript/make_script_dashing.sh
+++ shellscript/make_script_dashing.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cd ~/ros2/src/ros2_test/launch
-find . -type f -exec sed -i "s/executable=/node_executable=/g" {} \;
-find . -type f -exec sed -i "s/name=/node_name=/g" {} \;
-find . -type f -exec sed -i "s/namespace=/node_namespace=/g" {} \;
+git stash
+git stash drop stash\@\{0\}
+find . -type f -exec sed -i "s/ executable=/ node_executable=/g" {} \;
+find . -type f -exec sed -i "s/ name=/ node_name=/g" {} \;
+find . -type f -exec sed -i "s/ namespace=/ node_namespace=/g" {} \;
