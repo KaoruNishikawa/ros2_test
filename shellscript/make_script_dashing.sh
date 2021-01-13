fdf940a4db984611b5ece55dd5cafec9931a9bbb
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-13 15:22:26 +0900

    make net_count test automatic

--- shellscript/make_script_dashing.sh
+++ shellscript/make_script_dashing.sh
@@ -6,3 +6,4 @@ git stash drop stash\@\{0\}
 find . -type f -exec sed -i "s/ executable=/ node_executable=/g" {} \;
 find . -type f -exec sed -i "s/ name=/ node_name=/g" {} \;
 find . -type f -exec sed -i "s/ namespace=/ node_namespace=/g" {} \;
+cd ../shellscript
