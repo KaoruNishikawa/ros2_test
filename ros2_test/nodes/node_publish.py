0c58567e31a8411f27eb13e4721c273cb079bbc0
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-14 18:33:02 +0900

    change data amount

--- ros2_test/nodes/node_publish.py
+++ ros2_test/nodes/node_publish.py
@@ -4,7 +4,7 @@ import time
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 
 node_name = "node_publish"
 
@@ -24,6 +24,7 @@ class node_publish(Node):
         total_pairs = int(self.declare_parameter('total_pairs').value)
         if self.group < 90:
             self.num = (self.NUM + self.group * nodes_per_group) % total_pairs
+            self.data_pub = self.create_publisher(String, f"/test/data_{self.num:03d}", 1)
         else:
             self.num = 900 + self.group
         self.pub = self.create_publisher(Float64, f"/test/no{self.num:03d}", 1)
@@ -34,6 +35,10 @@ class node_publish(Node):
         msg = Float64()
         msg.data = float(time.time())
         self.pub.publish(msg)
+        if self.num < 90:
+            data = String()
+            data.data = 'a' * 1000
+            self.data_pub.publish(data)
 
 
 def main(args=None):
