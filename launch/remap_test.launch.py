f43da8c2be98b5591b8d03e2f07427212f4da9e7
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-14 13:07:17 +0900

    add remap test

---
+++ launch/remap_test.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    ld.add_action(
+        Node(
+            package='ros2_test',
+            executable='exec_remap_test',
+            parameters=[
+                {'number': 999},
+            ],
+            remappings=[
+                ('topic_name', 'remapped'),
+                ('/node_999_1', '/NNN'),
+            ],
+            namespace='/test',
+        )
+    )
