1f7ce111b61c33dcaf75d62336fb08a41b4148a8
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-14 13:45:10 +0900

    modify parameter specification

--- launch/remap_test.launch.py
+++ launch/remap_test.launch.py
@@ -8,11 +8,11 @@ def generate_launch_description():
             package='ros2_test',
             executable='exec_remap_test',
             parameters=[
-                {'number': 999},
+                {'remap_test:number': 999},
             ],
             remappings=[
                 ('topic_name', 'remapped'),
-                ('remap_tests:node_999_1', 'NNN'),
+                ('remap_test:node_999_1', 'NNN'),
             ],
             namespace='/test',
         )
