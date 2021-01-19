630459ea3db6944e0e932ea7c16ad6c53936b741
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-01-19 19:52:20 +0900

    add non-ROS netcount checker

---
+++ netcount.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import time
+import psutil
+import os
+
+
+def main():
+    with open(f"{os.path.environ['HOME']}/Documents/netcount.txt") as f:
+        for i in range(50):
+            netcount = psutil.net_io_counters()
+            sent = netcount.bytes_sent
+            recv = netcount.bytes_recv
+            f.write(sent, " ", recv)
+    return
+
+
+if __name__ == "__main__":
+    pass
