f66aa68f8ecd756e89ed0144c904d3f03c623cc3
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-06-25 16:08:37 +0000

    modify error handling

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 
+IFS='/' read -r -a array <<< "$SHELL"
+rc_path="$HOME/.${array[-1]}rc"
+
+
 if ! type poetry > /dev/null 2>&1
 then
     echo "Installing poetry"
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-    echo '# poetry' >> ~/.bashrc
-    echo '# poetry' >> ~/.zshrc
-    echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
-    echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.zshrc
+    echo '' >> ${rc_path}
+    echo '# poetry' >> ${rc_path}
+    echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ${rc_path}
     echo "Poetry successfully installed"
+    export PATH="$HOME/.poetry/bin:$PATH"
+    echo "Restart the shell to use poetry"
 fi
-
-
-IFS='/' read -r -a array <<< "$SHELL"
-source "$HOME/.${array[-1]}rc"
 
 
 package_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -26,12 +27,29 @@ poetry config virtualenvs.create false --local
 
 if ! poetry check > /dev/null 2>&1
 then
+    echo "Initialize virtual environment"
     poetry init
+    echo "Virtual environment created"
+else
+    echo "Valid pyproject.toml exists"
 fi
 
 
-if ! poetry install -q
+if poetry install
 then
-    poetry update -q
+    echo "Successfully installed the dependencies"
+else
+    echo "====================================="
+    echo "- Installation failed, try updating -"
+    echo "====================================="
+
+    if poetry update
+    then
+        echo "Successfully updated the dependencies"
+    else
+        echo "========================================="
+	echo "- poetry failed to resolve dependencies -"
+        echo "========================================="
+    fi
 fi
 
