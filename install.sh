5e0c2c42a37ffccfe9dea5f0993b027dee615693
KaoruNishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-06-27 15:47:01 +0000

    check if path is set or not

--- install.sh
+++ install.sh
@@ -9,12 +9,18 @@ if ! type poetry > /dev/null 2>&1
 then
     echo "Installing poetry"
     curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-    echo '' >> ${rc_path}
-    echo '# poetry' >> ${rc_path}
-    echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ${rc_path}
-    echo "Poetry successfully installed"
-    export PATH="$HOME/.poetry/bin:$PATH"
-    echo "Restart the shell to use poetry"
+    if grep '.poetry/bin' ${rc_path} > /dev/null 2>&1
+    then
+        echo -e "\033[46mPoetry path has already been set\033[0m"
+    else
+        echo -e "\033[46mSetting path in ${rc_path}\033[0m"
+        echo '' >> ${rc_path}
+        echo '# poetry' >> ${rc_path}
+        echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ${rc_path}
+        echo -e "\033[46;1mPoetry successfully installed\033[0m"
+        export PATH="$HOME/.poetry/bin:$PATH"
+        echo -e "\033[46mRestart the shell to use poetry\033[0m"
+    fi
 fi
 
 
@@ -27,29 +33,33 @@ poetry config virtualenvs.create false --local
 
 if ! poetry check > /dev/null 2>&1
 then
-    echo "Initialize virtual environment"
+    echo -e "\033[46mInitialize virtual environment\033[0m"
     poetry init
-    echo "Virtual environment created"
+    echo -e "\033[46mVirtual environment created\033[0m"
 else
-    echo "Valid pyproject.toml exists"
+    echo -e "\033[46mValid pyproject.toml exists\033[0m"
 fi
 
 
 if poetry install
 then
-    echo "Successfully installed the dependencies"
+    echo -e "\033[46;1m===========================================\033[0m"
+    echo -e "\033[46;1m= Successfully installed the dependencies =\033[0m"
+    echo -e "\033[46;1m===========================================\033[0m"
 else
-    echo "====================================="
-    echo "- Installation failed, try updating -"
-    echo "====================================="
+    echo -e "\033[46;1m=====================================\033[0m"
+    echo -e "\033[46;1m= Installation failed, try updating =\033[0m"
+    echo -e "\033[46;1m=====================================\033[0m"
 
     if poetry update
     then
-        echo "Successfully updated the dependencies"
+        echo -e "\033[46;1m=========================================\033[0m"
+        echo -e "\033[46;1m= Successfully updated the dependencies =\033[0m"
+        echo -e "\033[46;1m=========================================\033[0m"
     else
-        echo "========================================="
-	echo "- poetry failed to resolve dependencies -"
-        echo "========================================="
+        echo -e "\033[46;1m=========================================\033[0m"
+	echo -e "\033[46;1m= poetry failed to resolve dependencies =\033[0m"
+        echo -e "\033[46;1m=========================================\033[0m"
     fi
 fi
 
